02417ff0c68243b8f3f01833fd42e315136ba551
Niki <nikitheofilaktidou@gmail.com>
2022-06-22 10:38:54 +0300

    c:

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,20 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using TodoApi;
-
+using TodoApi.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var connectionString = builder.Configuration.GetConnectionString("TodoApiDatabase");
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
+    opt.UseInMemoryDatabase("TodoList")); 
+builder.Services.AddTransient<DataSeeder>();
 //builder.Services.AddSwaggerGen(c =>
 //{
 //    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
 //});
 
 var app = builder.Build();
+//Seed Data
+
 
 // Configure the HTTP request pipeline.
 if (builder.Environment.IsDevelopment())
